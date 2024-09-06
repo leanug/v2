4b0f8ec588f50defa1bb8e7861908753da7b5ed3
Leandro Ubilla <leandroubilla@protonmail.com>
2024-09-06 20:41:32 -0300

    Projects update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,15 +1,15 @@
 import ponyflyCoverImage from '@/assets/images/projects/ponyfly-cover-min.png'
 
 export const projects = [
-  /*  {
-    title: "Layoutloom",
-    text: "Curated website designs across various categories for web developers and ui/ux designers. Design and development.",
-    badge: ["Next.js", "Personal project"],
-    image: layoutloomCoverImage,
-    imgAlt: "Layoutloom",
+  {
+    title: 'FeedbackRealm.com',
+    text: 'A user feedback management platform built to streamline feedback collection and prioritize feature development.',
+    badge: ['Next.js', 'MongoDB', 'Full-stack project'],
+    image: '',
+    imgAlt: '',
     link: '',
-    url: 'https://www.layoutloom.com/',
-  }, */
+    url: 'https://www.feedbackrealm.com/'
+  },
   {
     title: 'Ponyfly.net',
     text: 'Website design and development with WordPress',
