ccbabb9ec8261bf583857f394c62d5a2bc7c877d
Leandro Ubilla <leandroubilla@protonmail.com>
2024-09-12 00:19:19 -0300

    Ocalta project added

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,6 @@
 export const projects = [
   {
+    id: '0',
     title: 'The Twilight Code',
     text: 'A channel where I share tutorials and insights on React, TypeScript, Next.js, and other web development topics.',
     badge: [],
@@ -9,6 +10,7 @@ export const projects = [
     url: 'https://www.youtube.com/@thetwilightcode/'
   },
   {
+    id: '1',
     title: 'FeedbackRealm.com',
     text: 'A user feedback management platform built to streamline feedback collection and prioritize feature development.',
     badge: ['Next.js', 'MongoDB'],
@@ -16,5 +18,15 @@ export const projects = [
     imgAlt: '',
     link: '',
     url: 'https://www.feedbackrealm.com/'
+  }
+  {
+    id: '2',
+    title: 'Ocalta.com',
+    text: 'Ocalta is a construction company based in Monterrey with 20 years of experience in the industry. The website was designed to reflect their extensive expertise and build trust with potential clients.',
+    badge: ['UI/UX Design', 'Figma'],
+    image: '',
+    imgAlt: '',
+    link: '',
+    url: 'https://www.ocalta.com/'
   }
 ]
