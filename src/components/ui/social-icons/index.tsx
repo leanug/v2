7246d76db02386061e79115ce4e8b9ac595b91f7
Leandro Ubilla <leandroubilla@protonmail.com>
2024-10-03 17:05:03 -0300

    fix(icons): Icon height and width changed

--- src/components/ui/social-icons/index.tsx
+++ src/components/ui/social-icons/index.tsx
@@ -40,7 +40,7 @@ const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
 
   return (
     <a
-      className="text-sm"
+      className={`h-${size} w-${size}`}
       target="_blank"
       rel="noopener noreferrer"
       href={href}
